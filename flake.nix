5a245d14a7d5d46c8b500dfcb9152d0811b47012
jobin_nelson <jobinnelson369@gmail.com>
2025-06-01 18:31:05 +0530

    feat: add bacon & bacon-ls to nix flake

--- flake.nix
+++ flake.nix
@@ -3,22 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    crisidevBaconLs.url = "github:crisidev/bacon-ls";
+    crisidevBaconLs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        rustc
-        cargo
-      ];
-      shellHook = ''
-        echo "Let's learn Zero 2 production in Rust"
-      '';
+  outputs =
+    { self, nixpkgs, crisidevBaconLs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      bacon-ls = crisidevBaconLs.defaultPackage.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustc
+          cargo
+          rust-analyzer
+          rustfmt
+          clippy
+          bacon
+          bacon-ls
+          vscode-extensions.vadimcn.vscode-lldb.adapter
+        ];
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        shellHook = ''
+          echo "Let's learn Zero 2 production in Rust"
+        '';
+      };
     };
-  };
 }
-
